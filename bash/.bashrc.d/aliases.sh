75a2846669657eae3c4500ab4e0ddd4999f73806
James Fu <jyfu77@gmail.com>
2026-05-11 15:56:19 -0700

    moved configs for bash and ghostty

---
+++ bash/.bashrc.d/aliases.sh
@@ -0,0 +1,28 @@
+# enable color support
+alias ls='ls --color=auto'
+#alias dir='dir --color=auto'
+#alias vdir='vdir --color=auto'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias bashrc='nvim ~/.bashrc'
+alias ea='nvim ~/.bashrc.d/aliases.sh'
+alias reload='. ~/.bashrc'
+alias restart='exec bash'
+
+alias bashconfig='nvim ~/.bashrc.d'
+alias ghosttyconfig='nvim ~/.config/ghostty'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# ssh hosts
+alias csil="ssh fu77@csil.cs.ucsb.edu"
---
+++ bash/.bashrc.d/misc.sh
@@ -0,0 +1,59 @@
+# for using ble.sh 
+source /usr/share/blesh/ble.sh --noattach
+# for enabling vi editing mode
+set -o vi
+
+source /usr/share/git/completion/git-prompt.sh
+
+# colored prompt using forest theme (using raw ANSI for ble.sh compatibility)
+red=$'\e[31m'
+green=$'\e[32m'
+yellow=$'\e[33m'
+blue=$'\e[34m'
+purple=$'\e[35m'
+cyan=$'\e[36m'
+reset=$'\e[0m'
+
+PS1='\[$green\]\u@\h:\[$blue\]\w \[$cyan\]$(__git_ps1 " (%s)")\n\[$purple\]\$\[$reset\] '
+
+# colored GCC warnings and errors
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+export EDITOR='nvim'
+export VISUAL='nvim'
+
+# Attach ble.sh to the active session
+[[ ${BLE_VERSION-} ]] && ble-attach
